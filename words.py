496280df89d84f6a2224d9ce29c19a4601f3ada7
SConv11 <yuchenco@andrew.cmu.edu>
2026-04-15 18:41:17 -0400

    Added wordbank files and Wrote generateWordList function

--- words.py
+++ words.py
@@ -14,19 +14,20 @@ def generateWordList(numWords, hard=False, punc = False) -> list[str]:
     res = random.choices(wordbank, k=numWords)
 
     if punc:
-        print(res[0])
         res[0] = res[0][0].upper() + res[0][1:]
         for i in range(len(res)-1):
-            if random.random() < 0.3: res[i] += random.choice(['.', ',', '!', '?', ';', ':'])
-            if res[i][-1] in ['.', '!', '?']: res[i+1] = res[i+1][0].upper() + res[i+1][1:]
+            if random.random() < 0.3: 
+                res[i] += random.choice(['.', ',', '!', '?', ';', ':'])
+            if res[i][-1] in ['.', '!', '?']: 
+                res[i+1] = res[i+1][0].upper() + res[i+1][1:]
         res[-1] += random.choice(['.', '!', '?'])
 
     return res
 
 
-print(generateWordList(100, hard=True))
-print('--------------------------------------------------------------------')
-print(generateWordList(100))
-print('--------------------------------------------------------------------')
-print(generateWordList(100, punc=True))
+# print(generateWordList(100, hard=True))
+# print('--------------------------------------------------------------------')
+# print(generateWordList(100))
+# print('--------------------------------------------------------------------')
+# print(generateWordList(100, punc=True))
 
